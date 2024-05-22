6dbbbc0114dcc82805b317edbe3d08dcc8e9dc6c
ChuSeongJun <j949854@gmail.com>
2024-05-22 14:53:24 +0900

    Feat: 공통 컴포넌트까지 분리까지 완료

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,20 @@
-<script setup>
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const goAboutPage = () => router.push('/about');
-</script>
 <template>
   <h1>Home Page</h1>
   <button @click="goAboutPage">About 페이지로 이동</button>
+  <hr class="my-4" />
+  <AppGrid :items="items" v-slot="{ item }">
+    <AppCard>{{ item }}</AppCard>
+  </AppGrid>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
+const router = useRouter();
+const goAboutPage = () => {
+  router.push('/about');
+};
+const items = ref(['사과', '딸기', '포도', '바나나']);
+</script>
